3fecdd170440dcb3bebb5052fe513ca689afd45d
Cezar <iancu.m.cezar@gmail.com>
2021-04-27 16:22:01 +0300

    first commit,full project

---
+++ stock-bd/src/main/java/com/racheta/stockbd/model/Quote.java
@@ -0,0 +1,37 @@
+package com.racheta.stockbd.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "quotes", catalog = "test")
+public class Quote {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "user_name")
+    private String userName;
+
+    @Column(name = "quote")
+    private String quote;
+
+
+
+    public Quote(String userName, String quote) {
+        this.userName = userName;
+        this.quote = quote;
+    }
+
+
+}
---
+++ stock-bd/src/main/java/com/racheta/stockbd/model/Quotes.java
@@ -0,0 +1,21 @@
+package com.racheta.stockbd.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+import java.util.List;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Quotes {
+
+    private String userName;
+    private List<String> quotes;
+
+
+
+}
